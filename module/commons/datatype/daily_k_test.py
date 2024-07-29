9f8076af7b6ff8ced1b0988098678741a2ecbeb6
yanshuangchen <13862810574@163.com>
2024-07-29 17:47:52 +0800

    模块名称变动

--- module/commons/datatype/daily_k_test.py
+++ module/commons/datatype/daily_k_test.py
@@ -1,34 +1,40 @@
 import sys
 from datetime import datetime
-sys.path.append('.')
+
+sys.path.append(".")
 
 
 def daily_test():
     from module.commons.stock.stock import SessionLocal, engine, Base
-    from module.commons.datatype.daily_k import insert_daily_k, daily_k_StockData, get_daily_k
+    from module.commons.datatype.daily_k import (
+        insert_daily_k,
+        daily_k_StockData,
+        get_daily_k,
+    )
+
     Base.metadata.create_all(bind=engine)
     db = SessionLocal()
 
     # 生成一条记录
     data = daily_k_StockData(
         date=datetime.now(),
-        code=114514,
+        code="sh.114514",
         open=1.21,
         high=1.30,
         low=1.19,
         close=1.20,
         volume=1,
-        amount=1.2
+        amount=1.2,
     )
     # 插入数据库
     insert_daily_k(db=db, data=data)
 
     # 查询数据
-    return_data = get_daily_k(db=db, code=114514)
+    return_data = get_daily_k(db=db, code="sh.114514")
     print(return_data.date)
     print(return_data.code)
     print(return_data.open)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     daily_test()
